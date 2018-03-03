fa25424012f705e183bc93528af1d0c8366f6e0f
ArjithaVivekanandan <36363436+ArjithaVivekanandan@users.noreply.github.com>
2018-03-03 05:48:57 +0530

    29.cpp

---
+++ 29.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int mins,hours;
+void main()
+{
+cin>>min;
+hours=min/60;
+cout<<"Mins equivalent in hours and min"<<int(hours)<<"hours"<<"\t"<<int(min%60)<<"minutes";
+getch();
+}
