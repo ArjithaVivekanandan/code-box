db7c3141fbe5458d6b95a6ac5695645f086d74ff
ArjithaVivekanandan <36363436+ArjithaVivekanandan@users.noreply.github.com>
2018-03-04 22:01:54 +0530

    ha.cpp

---
+++ ha.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+ void print_repeat(int array[],int size[])
+ {
+ int i;
+ cout<<"Repeating elements are:";
+ for(i=0;i<size;i++)
+ {
+ if(array[abs(array[i])]>0)
+  array[abs(array[i])] = -array[abs(array[i])];
+    else
+      printf(" %d ", abs(array[i]));
+  }         
+}     
+ 
+int main()
+{
+  int array[1000];
+  int array_size = sizeof(array)/sizeof(array[0]);
+  printRepeating(array, array_size);
+  getchar();
+  return 0;
+}
