399c3937fb76fec916e39865aa24dc96cf76a4cd
ArjithaVivekanandan <36363436+ArjithaVivekanandan@users.noreply.github.com>
2018-05-23 14:29:26 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 int main()
 {
-int num,fact=1;
+int num,fact=1,i;
 cin>>num;
-if(num!=0)
+if((num!=0&&num!=1)&&(num>0||num<0))
 {
 for(i=1;i<=num;i++)
 fact=fact*i;
+  cout<<fact;
 }
-
+else
+  cout<<"Invalid";
 return 0;
 }
