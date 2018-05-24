f7f9125345284eb17df8de072571a3e6f23dbc71
ArjithaVivekanandan <36363436+ArjithaVivekanandan@users.noreply.github.com>
2018-05-24 14:41:41 +0530

    Create maxnum.cpp

---
+++ maxnum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+    int a[10],i,max;
+   
+    for(i=0;i<10;i++)
+    {
+         cin>>a[i];
+    }
+    max=a[0];
+    for(i=1;i<10;i++)
+{
+    if(max<a[i])
+    max=a[i];
+}
+    cout<<max;
+    return 0;
+}
