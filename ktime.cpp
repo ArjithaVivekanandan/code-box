d3838e426dfa7840ea63feba14a27ac80218ea93
ArjithaVivekanandan <36363436+ArjithaVivekanandan@users.noreply.github.com>
2018-05-24 15:22:49 +0530

    Create ktime.cpp

---
+++ ktime.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+   string S;
+   int i,K;
+   
+   cin>>S;
+   
+   cin>>K;
+   for(i=0;i<K;i++)
+   {
+   cout<<S<<" ";
+   }
+    return 0;
+}
