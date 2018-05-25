479a99edece5afd52e5304f386a24ecc8f9be423
ArjithaVivekanandan <36363436+ArjithaVivekanandan@users.noreply.github.com>
2018-05-25 16:13:19 +0530

    Update biitswaap.cpp

--- biitswaap.cpp
+++ biitswaap.cpp
@@ -12,9 +12,9 @@ void swap(int *a,int *b)
 }
 int main()
 {
-    int a,b;
-    cin>>a>>b;
-    swap(&a,&b);
-    cout<<a<<" "<<b;
+  int e,f;
+    cin>>e>>f;
+    swap(&e,&f);
+    cout<<e<<f;
    return 0;
 }
